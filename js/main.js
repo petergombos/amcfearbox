00f4161fb54dc75542bc4490563e638f34ff6ad8
Márton Borlay <marton.borlay@kriek.hu>
2015-08-05 12:49:29 +0200

    New video

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
      var hash = (window.location.hash + "").replace("#","");
     if (hash) {
         var src = $("iframe.video").attr("src");
-        $("iframe.video").attr("src", src.replace("135038204", hash));
+        $("iframe.video").attr("src", src.replace("135445856", hash));
 
         $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/'+hash+'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
     } else {
