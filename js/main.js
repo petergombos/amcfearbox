c17140644a24505df81add4bb621e8b7476a3b90
Márton Borlay <marton.borlay@kriek.hu>
2015-08-06 12:41:52 +0200

    Share btn

--- js/main.js
+++ js/main.js
@@ -5,7 +5,24 @@ $(document).ready(function () {
         $("iframe.video").attr("src", src.replace("135445856", hash));
 
         $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/'+hash+'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
+        $("iframe.video").after(' <button onclick="share();">share</button>');
     } else {
         $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/135445856" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
+        $("iframe.video").after(' <button onclick="share();">share</button>');
     }
 });
+
+function share() {
+	FB.ui({
+            method: 'feed',
+            picture: 'https://amcfearbox.com/img/cover.jpg',
+            link: 'http://amc-redirect.kriek.io/134951164',
+            name: 'AMC Fearbox',
+            description: 'This is a description',
+            source: 'https://player.vimeo.com/video/134951164',
+            type: 'video',
+      });
+}
+
+
+
