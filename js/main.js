5fae5e7c6b570ab04cda108b80f1ec2d54ef6b98
Márton Borlay <marton.borlay@kriek.hu>
2015-08-06 13:41:53 +0200

    share

--- js/main.js
+++ js/main.js
@@ -4,22 +4,23 @@ $(document).ready(function () {
         var src = $("iframe.video").attr("src");
         $("iframe.video").attr("src", src.replace("135445856", hash));
 
-        $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/'+hash+'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
-        $("iframe.video").after(' <button onclick="share();">share</button>');
-    } else {
-        $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/135445856" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
-        $("iframe.video").after(' <button onclick="share();">share</button>');
+        // $("iframe.video").after(' <div class="fb-like" data-href="http://amc-redirect.kriek.io/'+hash+'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>');
     }
+    
+    $("iframe.video").after(' <button onclick="share();" class="share">Megosztás</button>');
 });
 
 function share() {
+	var hash = (window.location.hash + "").replace("#","");
 	FB.ui({
             method: 'feed',
-            picture: 'https://amcfearbox.com/img/cover.jpg',
-            link: 'http://amc-redirect.kriek.io/134951164',
+            picture: 'http://amcfearbox.com/img/cover.jpg',
+            link: 'http://amc-redirect.kriek.io/'+hash,
+            // link: 'http://google.com',
             name: 'AMC Fearbox',
-            description: 'This is a description',
-            source: 'https://player.vimeo.com/video/134951164',
+            caption: "AMC",
+            // description: '',
+            source: 'https://vimeo.com/moogaloop.swf?clip_id=' + hash,
             type: 'video',
       });
 }
